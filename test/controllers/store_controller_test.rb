197a0dfc9e321aab2625a415f7f5360a624a8f98
Zach Toolson <zach.toolson@gmail.com>
2015-08-08 08:50:37 -0600

    add store page to show products to buyers. make this the homepage of the application and style it

---
+++ test/controllers/store_controller_test.rb
@@ -0,0 +1,9 @@
+require 'test_helper'
+
+class StoreControllerTest < ActionController::TestCase
+  test "should get index" do
+    get :index
+    assert_response :success
+    assert_not_nil assigns(:products)
+  end
+end
