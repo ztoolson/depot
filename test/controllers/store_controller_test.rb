e0ebe513c262c193fe54dd4d1042c0d07554dd0d
Zach Toolson <zach.toolson@gmail.com>
2015-08-08 09:13:44 -0600

    write store controller tests for elements on the rendered page

--- test/controllers/store_controller_test.rb
+++ test/controllers/store_controller_test.rb
@@ -4,6 +4,10 @@ class StoreControllerTest < ActionController::TestCase
   test "should get index" do
     get :index
     assert_response :success
-    assert_not_nil assigns(:products)
+
+    # Test elements on the page
+    assert_select "#columns #side a", minimum: 4
+    assert_select "h3", products(:valid_product).title
+    assert_select ".price", /\$[,\d]+\.\d\d/
   end
 end
