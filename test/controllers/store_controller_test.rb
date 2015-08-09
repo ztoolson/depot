467358dd04924a096bf14ffcfeb765a2ec662d23
Zach Toolson <zach.toolson@gmail.com>
2015-08-09 12:50:48 -0600

    write tests for javascript code

--- test/controllers/store_controller_test.rb
+++ test/controllers/store_controller_test.rb
@@ -10,4 +10,11 @@ class StoreControllerTest < ActionController::TestCase
     assert_select "h3", products(:valid_product).title
     assert_select ".price", /\$[,\d]+\.\d\d/
   end
+
+  test "markup needed for store.js coffee is in place" do
+    get :index
+
+    assert_select ".store .entry > img", 2
+    assert_select ".entry input[type=submit]", 2
+  end
 end
